78af912e4d15944c888630f6635832799a876820
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-05-11 07:25:03 -0400

    update license and url

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject flyingmachine/serialize "0.1.0"
   :description "Define rules for serializing entities"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
+  
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :url "https://github.com/flyingmachine/serialize")
   
